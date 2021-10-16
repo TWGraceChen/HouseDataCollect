6bea70b09bfcba59610d31d9a9f2ba9833cbb23b
Grace Chen <gracechen@gracechen.io>
2021-10-17 02:48:11 +0800

    remove year bound in house data

--- house.py
+++ house.py
@@ -1,12 +1,18 @@
 import requests, zipfile, io
 
-for y in range(101,110):
-    for s in range(1,5):
+y = 101
+stop = False
+while stop != True:
+    for s in [1,2,3,4]:
         period = "%sS%s"%(y,s)
-        print("downing:"+period)
+        print("downloading:"+period)
         url = "https://plvr.land.moi.gov.tw//DownloadSeason?season=" + period + "&type=zip&fileName=lvr_landcsv.zip"
-        r = requests.get(url)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall("./data/house/"+period)
-
+        try:
+            r = requests.get(url)
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall("./data/house/"+period)
+        except:
+            stop = True
+            break
+    y = y + 1
 print("====Finish====")
